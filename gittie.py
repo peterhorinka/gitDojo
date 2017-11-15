b322500ec8f3e2122fb8f27b4bf4313288c36e61
Horinka Peter <horinka.peter@gmail.com>
2017-11-15 09:54:29 +0100

    lol

--- gittie.py
+++ gittie.py
@@ -8,8 +8,4 @@ def joke():
     print("Kopp kopp. Ki az? Senki")
 
 def shout():
-<<<<<<< HEAD
     print("IM SHOUTING")
-=======
-    print("VAAA3234")
->>>>>>> ea08d3312db525f3177b2a41d94e46266d72c08e
